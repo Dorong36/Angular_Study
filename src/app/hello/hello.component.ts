0e0b6650b0b8906c38ab6cd022c95c7ecc1402b6
유도영 <dbehdud35@naver.com>
2023-08-17 23:46:45 +0900

    0817

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'hello', // 통상 app용으로 만들어졌다고 알리기 위해 app-xxx로 쓰긴함
+    templateUrl : './hello.component.html',
+    styleUrls : ['./hello.component.scss']
+})
+export class HelloComponent {
+
+}
